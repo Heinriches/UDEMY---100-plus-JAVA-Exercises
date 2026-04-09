036be3886a7a4f4fa5ee5947137b48e4fc05648e
Heinriches <heydrich.institut@gmail.com>
2026-04-09 20:18:10 -0300

    Create Exercise_001.java

---
+++ Exercise_001.java
@@ -0,0 +1,9 @@
+public class Exercise_001 {
+    // TODO: Implement the printMessage method
+    public static void main (String[] args){
+        printMessage();
+    }
+    public static void printMessage() {
+        System.out.println("Hello, World!");
+    }
+}
